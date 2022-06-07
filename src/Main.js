5cbafba3878fe2531d4763b18e67fc31899d83d5
Joonatan Merenluoto <joonatan.merenluoto@gmail.com>
2022-06-07 10:05:54 +0300

    Remove notes preview

--- src/Main.js
+++ src/Main.js
@@ -45,19 +45,8 @@ function Main({activeNote, onUpdateNote})
 
                 </div>
 
-                    <div className="app-main-note-preview">
-
-                        <h1 className="preview-title">{activeNote.title}</h1>
-
-                        <div className="markdown-preview">
-
-                        {activeNote.body}
-
-                        </div>
 
                      </div>
-
-            </div>
 }
 
 export default Main;
